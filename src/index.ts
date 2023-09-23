5f0e0e1378ab0d11472281ce62312361c2676a94
Migush <Migushthe2nd@users.noreply.github.com>
2023-09-23 16:33:27 +0200

    Export ProsodyOptions type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {OUTPUT_FORMAT} from "./OUTPUT_FORMAT";
 import {MsEdgeTTS} from "./MsEdgeTTS";
 
-export {MsEdgeTTS, Voice} from "./MsEdgeTTS"
+export {MsEdgeTTS, Voice, ProsodyOptions} from "./MsEdgeTTS"
 export {OUTPUT_FORMAT} from "./OUTPUT_FORMAT"
 export {PITCH} from "./PITCH"
 export {RATE} from "./RATE"
