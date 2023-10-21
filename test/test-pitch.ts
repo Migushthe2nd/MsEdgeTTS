1ab176b8b232b75673133f5a5a1d0531438da8d7
Migush <Migushthe2nd@users.noreply.github.com>
2023-10-21 11:00:39 +0200

    Update tests

--- test/test-pitch.ts
+++ test/test-pitch.ts
@@ -1,9 +1,8 @@
 import {MsEdgeTTS, OUTPUT_FORMAT} from "../dist";
 
 (async () => {
-    const tts = new MsEdgeTTS(null, false);
+    const tts = new MsEdgeTTS();
     await tts.setMetadata("en-US-AriaNeural", OUTPUT_FORMAT.WEBM_24KHZ_16BIT_MONO_OPUS);
-    const filePath = await tts.toFile("./example_audio.webm", "Hi, how are you?");
+    const filePath = await tts.toFile("./example_audio_pitched.webm", "Hi, how are you?", {rate: 0.5, pitch: "+3st"});
     console.log("Done!", filePath);
-    tts.close();
 })();
