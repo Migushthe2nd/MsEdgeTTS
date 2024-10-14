e7d62691e5af8d068b033156d97985cef707bea4
Migush <Migushthe2nd@users.noreply.github.com>
2024-10-14 15:48:54 +0200

    Separate message types more

--- src/MsEdgeTTS.ts
+++ src/MsEdgeTTS.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
-import WebSocket from "isomorphic-ws";
-import {Buffer} from "buffer";
-import randomBytes from "randombytes";
-import {OUTPUT_FORMAT} from "./OUTPUT_FORMAT";
-import {Readable} from "stream";
-import * as fs from "fs";
-import {Agent} from "http";
-import {PITCH} from "./PITCH";
-import {RATE} from "./RATE";
-import {VOLUME} from "./VOLUME";
+import axios from "axios"
+import WebSocket from "isomorphic-ws"
+import {Buffer} from "buffer/" // slash is important for browser compatibility
+import randomBytes from "randombytes"
+import {OUTPUT_FORMAT} from "./OUTPUT_FORMAT"
+import {Readable} from "stream"
+import * as fs from "fs"
+import {Agent} from "http"
+import {PITCH} from "./PITCH"
+import {RATE} from "./RATE"
+import {VOLUME} from "./VOLUME"
 
 export type Voice = {
     Name: string;
@@ -26,37 +26,48 @@ export class ProsodyOptions {
      * Can be any {@link PITCH}, or a relative frequency in Hz (+50Hz), a relative semitone (+2st), or a relative percentage (+50%).
      * [SSML documentation](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/speech-synthesis-markup-voice#:~:text=Optional-,pitch,-Indicates%20the%20baseline)
      */
-    pitch?: PITCH | string = "+0Hz";
+    pitch?: PITCH | string = "+0Hz"
     /**
      * The rate to use.
      * Can be any {@link RATE}, or a relative number (0.5), or string with a relative percentage (+50%).
      * [SSML documentation](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/speech-synthesis-markup-voice#:~:text=Optional-,rate,-Indicates%20the%20speaking)
      */
-    rate?: RATE | string | number = 1.0;
+    rate?: RATE | string | number = 1.0
     /**
      * The volume to use.
      * Can be any {@link VOLUME}, or an absolute number (0, 100), a string with a relative number (+50), or a relative percentage (+50%).
      * [SSML documentation](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/speech-synthesis-markup-voice#:~:text=Optional-,volume,-Indicates%20the%20volume)
      */
-    volume?: VOLUME | string | number = 100.0;
+    volume?: VOLUME | string | number = 100.0
+}
+
+enum messageTypes {
+    TURN_START = "turn.start",
+    TURN_END = "turn.end",
+    RESPONSE = "response",
+    SPEECH_CONFIG = "speech.config",
+    AUDIO_METADATA = "audio.metadata",
+    AUDIO = "audio",
+    SSML = "ssml",
 }
 
 export class MsEdgeTTS {
-    static OUTPUT_FORMAT = OUTPUT_FORMAT;
-    private static TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4";
-    private static VOICES_URL = `https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=${MsEdgeTTS.TRUSTED_CLIENT_TOKEN}`;
-    private static SYNTH_URL = `wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=${MsEdgeTTS.TRUSTED_CLIENT_TOKEN}`;
-    private static BINARY_DELIM = "Path:audio\r\n";
-    private static VOICE_LANG_REGEX = /\w{2}-\w{2}/;
-    private readonly _enableLogger;
-    private readonly _isBrowser: boolean;
-    private _ws: WebSocket;
-    private _voice;
-    private _voiceLocale;
-    private _outputFormat;
-    private _streams: { [key: string]: Readable } = {};
-    private _startTime = 0;
-    private readonly _agent: Agent;
+    static OUTPUT_FORMAT = OUTPUT_FORMAT
+    private static TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
+    private static VOICES_URL = `https://speech.platform.bing.com/consumer/speech/synthesize/readaloud/voices/list?trustedclienttoken=${MsEdgeTTS.TRUSTED_CLIENT_TOKEN}`
+    private static SYNTH_URL = `wss://speech.platform.bing.com/consumer/speech/synthesize/readaloud/edge/v1?TrustedClientToken=${MsEdgeTTS.TRUSTED_CLIENT_TOKEN}`
+    private static JSON_XML_DELIM = "\r\n\r\n"
+    private static AUDIO_DELIM = "Path:audio\r\n"
+    private static VOICE_LANG_REGEX = /\w{2}-\w{2}/
+    private readonly _enableLogger
+    private readonly _isBrowser: boolean
+    private _ws: WebSocket
+    private _voice
+    private _voiceLocale
+    private _outputFormat
+    private _streams: { [key: string]: { audio: Readable, metadata: Readable } } = {}
+    private _startTime = 0
+    private readonly _agent: Agent
 
     private _log(...o: any[]) {
         if (this._enableLogger) {
@@ -71,34 +82,34 @@ export class MsEdgeTTS {
      * @param enableLogger=false whether to enable the built-in logger. This logs connections inits, disconnects, and incoming data to the console
      */
     public constructor(agent?: Agent, enableLogger: boolean = false) {
-        this._agent = agent;
-        this._enableLogger = enableLogger;
-        this._isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined";
+        this._agent = agent
+        this._enableLogger = enableLogger
+        this._isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined"
     }
 
     private async _send(message) {
         for (let i = 1; i <= 3 && this._ws.readyState !== this._ws.OPEN; i++) {
             if (i == 1) {
-                this._startTime = Date.now();
+                this._startTime = Date.now()
             }
-            this._log("connecting: ", i);
-            await this._initClient();
+            this._log("connecting: ", i)
+            await this._initClient()
         }
         this._ws.send(message, () => {
-            this._log("<- sent message: ", message);
-        });
+            this._log("<-", message)
+        })
     }
 
     private _initClient() {
         this._ws = this._isBrowser
             ? new WebSocket(MsEdgeTTS.SYNTH_URL)
-            : new WebSocket(MsEdgeTTS.SYNTH_URL, {agent: this._agent});
+            : new WebSocket(MsEdgeTTS.SYNTH_URL, {agent: this._agent})
 
-        this._ws.binaryType = "arraybuffer";
+        this._ws.binaryType = "arraybuffer"
         return new Promise((resolve, reject) => {
             this._ws.onopen = () => {
                 this._log("Connected in", (Date.now() - this._startTime) / 1000, "seconds")
-                this._send(`Content-Type:application/json; charset=utf-8\r\nPath:speech.config\r\n\r\n
+                this._send(`Content-Type:application/json; charset=utf-8\r\nPath:${messageTypes.SPEECH_CONFIG}${MsEdgeTTS.JSON_XML_DELIM}
                     {
                         "context": {
                             "synthesis": {
@@ -112,42 +123,60 @@ export class MsEdgeTTS {
                             }
                         }
                     }
-                `).then(resolve);
-            };
+                `).then(resolve)
+            }
             this._ws.onmessage = (m) => {
-                const buffer = Buffer.from(m.data as ArrayBuffer);
+                const buffer = Buffer.from(m.data as ArrayBuffer)
                 const message = buffer.toString()
-                const requestId = /X-RequestId:(.*?)\r\n/gm.exec(message)[1];
-                if (message.includes("Path:turn.start")) {
+
+                const requestId = /X-RequestId:(.*?)\r\n/gm.exec(message)[1]
+
+                if (message.includes(`Path:${messageTypes.TURN_START}`)) {
                     // start of turn, ignore
-                } else if (message.includes("Path:turn.end")) {
+                    this._log("->", message)
+                } else if (message.includes(`Path:${messageTypes.TURN_END}`)) {
                     // end of turn, close stream
-                    this._streams[requestId].push(null);
-                } else if (message.includes("Path:response")) {
+                    this._log("->", message)
+                    this._streams[requestId].audio.push(null)
+                } else if (message.includes(`Path:${messageTypes.RESPONSE}`)) {
                     // context response, ignore
-                } else if (message.includes("Path:audio") && m.data instanceof ArrayBuffer) {
-                    this._pushAudioData(buffer, requestId)
+                    this._log("->", message)
+                } else if (message.includes(`Path:${messageTypes.AUDIO_METADATA}`)) {
+                    // audio metadata, wordboundary/sentenceboundary
+                    const dataStartIndex = buffer.indexOf(MsEdgeTTS.JSON_XML_DELIM) + MsEdgeTTS.JSON_XML_DELIM.length
+                    const data = buffer.subarray(dataStartIndex)
+
+                    this._log("->", message)
+                    this._pushMetadata(data, requestId)
+                } else if (message.includes(`Path:${messageTypes.AUDIO}`) && m.data instanceof ArrayBuffer) {
+                    const dataStartIndex = buffer.indexOf(MsEdgeTTS.AUDIO_DELIM) + MsEdgeTTS.AUDIO_DELIM.length
+                    const headers = buffer.subarray(0, dataStartIndex).toString()
+                    const data = buffer.subarray(dataStartIndex)
+
+                    this._log("->", headers)
+                    this._pushAudioData(data, requestId)
                 } else {
-                    this._log("UNKNOWN MESSAGE", message);
+                    this._log("->", "UNKNOWN MESSAGE", message)
                 }
             }
             this._ws.onclose = () => {
                 this._log("disconnected after:", (Date.now() - this._startTime) / 1000, "seconds")
                 for (const requestId in this._streams) {
-                    this._streams[requestId].push(null);
+                    this._streams[requestId].audio.push(null)
                 }
             }
             this._ws.onerror = function (error) {
-                reject("Connect Error: " + error);
-            };
-        });
+                reject("Connect Error: " + JSON.stringify(error, null, 2))
+            }
+        })
     }
 
-    private _pushAudioData(audioBuffer: Buffer, requestId: string) {
-        const audioStartIndex = audioBuffer.indexOf(MsEdgeTTS.BINARY_DELIM) + MsEdgeTTS.BINARY_DELIM.length;
-        const audioData = audioBuffer.subarray(audioStartIndex);
-        this._streams[requestId].push(audioData);
-        this._log("received audio chunk, size: ", audioData?.length)
+    private _pushAudioData(data: Uint8Array, requestId: string) {
+        this._streams[requestId].audio.push(data)
+    }
+
+    private _pushMetadata(data: Uint8Array, requestId: string) {
+        this._streams[requestId].metadata.push(data)
     }
 
     private _SSMLTemplate(input: string, options: ProsodyOptions = {}): string {
@@ -159,7 +188,7 @@ export class MsEdgeTTS {
                         ${input}
                     </prosody> 
                 </voice>
-            </speak>`;
+            </speak>`
     }
 
     /**
@@ -170,8 +199,8 @@ export class MsEdgeTTS {
         return new Promise((resolve, reject) => {
             axios.get(MsEdgeTTS.VOICES_URL)
                 .then((res) => resolve(res.data))
-                .catch(reject);
-        });
+                .catch(reject)
+        })
     }
 
     /**
@@ -184,40 +213,40 @@ export class MsEdgeTTS {
      * @param voiceLocale (optional) any voice locale that is supported by the voice. See the list of all voices for compatibility. If not provided, the locale will be inferred from the `voiceName`
      */
     async setMetadata(voiceName: string, outputFormat: OUTPUT_FORMAT, voiceLocale?: string) {
-        const oldVoice = this._voice;
-        const oldVoiceLocale = this._voiceLocale;
-        const oldOutputFormat = this._outputFormat;
+        const oldVoice = this._voice
+        const oldVoiceLocale = this._voiceLocale
+        const oldOutputFormat = this._outputFormat
 
-        this._voice = voiceName;
-        this._voiceLocale = voiceLocale;
+        this._voice = voiceName
+        this._voiceLocale = voiceLocale
         if (!this._voiceLocale) {
-            const voiceLangMatch = MsEdgeTTS.VOICE_LANG_REGEX.exec(this._voice);
-            if (!voiceLangMatch) throw new Error("Could not infer voiceLocale from voiceName!");
-            this._voiceLocale = voiceLangMatch[0];
+            const voiceLangMatch = MsEdgeTTS.VOICE_LANG_REGEX.exec(this._voice)
+            if (!voiceLangMatch) throw new Error("Could not infer voiceLocale from voiceName!")
+            this._voiceLocale = voiceLangMatch[0]
         }
-        this._outputFormat = outputFormat;
+        this._outputFormat = outputFormat
 
         const changed = oldVoice !== this._voice
             || oldVoiceLocale !== this._voiceLocale
-            || oldOutputFormat !== this._outputFormat;
+            || oldOutputFormat !== this._outputFormat
 
         // create new client
         if (changed || this._ws.readyState !== this._ws.OPEN) {
             this._startTime = Date.now()
-            await this._initClient();
+            await this._initClient()
         }
     }
 
     private _metadataCheck() {
         if (!this._ws) throw new Error(
-            "Speech synthesis not configured yet. Run setMetadata before calling toStream or toFile.");
+            "Speech synthesis not configured yet. Run setMetadata before calling toStream or toFile.")
     }
 
     /**
      * Close the WebSocket connection.
      */
     close() {
-        this._ws.close();
+        this._ws.close()
     }
 
     /**
@@ -229,7 +258,7 @@ export class MsEdgeTTS {
      * @returns {Promise<string>} - a `Promise` with the full filepath
      */
     toFile(path: string, input: string, options?: ProsodyOptions): Promise<string> {
-        return this._rawSSMLRequestToFile(path, this._SSMLTemplate(input, options));
+        return this._rawSSMLRequestToFile(path, this._SSMLTemplate(input, options))
     }
 
     /**
@@ -240,8 +269,8 @@ export class MsEdgeTTS {
      * @returns {Readable} - a `stream.Readable` with the audio data
      */
     toStream(input: string, options?: ProsodyOptions): Readable {
-        const {stream} = this._rawSSMLRequest(this._SSMLTemplate(input, options));
-        return stream;
+        const {audioStream} = this._rawSSMLRequest(this._SSMLTemplate(input, options))
+        return audioStream
     }
 
     /**
@@ -252,7 +281,7 @@ export class MsEdgeTTS {
      * @returns {Promise<string>} - a `Promise` with the full filepath
      */
     rawToFile(path: string, requestSSML: string): Promise<string> {
-        return this._rawSSMLRequestToFile(path, requestSSML);
+        return this._rawSSMLRequestToFile(path, requestSSML)
     }
 
     /**
@@ -262,51 +291,62 @@ export class MsEdgeTTS {
      * @returns {Readable} - a `stream.Readable` with the audio data
      */
     rawToStream(requestSSML: string): Readable {
-        const {stream} = this._rawSSMLRequest(requestSSML);
-        return stream;
+        const {audioStream} = this._rawSSMLRequest(requestSSML)
+        return audioStream
     }
 
     private _rawSSMLRequestToFile(path: string, requestSSML: string): Promise<string> {
         return new Promise(async (resolve, reject) => {
-            const {stream, requestId} = this._rawSSMLRequest(requestSSML);
+            const {audioStream, requestId} = this._rawSSMLRequest(requestSSML)
 
-            const writableFile = stream.pipe(fs.createWriteStream(path));
+            const writableFile = audioStream.pipe(fs.createWriteStream(path))
 
             writableFile.once("close", async () => {
                 if (writableFile.bytesWritten > 0) {
-                    resolve(path);
+                    resolve(path)
                 } else {
-                    fs.unlinkSync(path);
-                    reject("No audio data received");
+                    fs.unlinkSync(path)
+                    reject("No audio data received")
                 }
-            });
+            })
 
-            stream.on("error", (e) => {
-                stream.destroy();
-                reject(e);
-            });
-        });
+            audioStream.on("error", (e) => {
+                audioStream.destroy()
+                reject(e)
+            })
+        })
     }
 
-    private _rawSSMLRequest(requestSSML: string): {stream: Readable, requestId: string} {
-        this._metadataCheck();
+    private _rawSSMLRequest(requestSSML: string): { audioStream: Readable, metadataStream: Readable, requestId: string } {
+        this._metadataCheck()
 
-        const requestId = randomBytes(16).toString("hex");
-        const request = `X-RequestId:${requestId}\r\nContent-Type:application/ssml+xml\r\nPath:ssml\r\n\r\n
-                ` + requestSSML.trim();
+        const requestId = randomBytes(16).toString("hex")
+        const request = `X-RequestId:${requestId}\r\nContent-Type:application/ssml+xml\r\nPath:${messageTypes.SSML}${MsEdgeTTS.JSON_XML_DELIM}` + requestSSML.trim()
         // https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup
-        const self = this;
-        const stream = new Readable({
+        const self = this
+        const audioStream = new Readable({
             read() {
             },
             destroy(error: Error | null, callback: (error: (Error | null)) => void) {
-                delete self._streams[requestId];
-                callback(error);
+                delete self._streams[requestId]
+                callback(error)
             },
-        });
-        this._streams[requestId] = stream;
-        this._send(request).then();
-        return {stream, requestId};
+        })
+        const metadataStream = new Readable({
+            read() {
+            },
+        })
+        audioStream.once("close", () => {
+            audioStream.destroy()
+            metadataStream.destroy()
+        })
+
+        this._streams[requestId] = {
+            audio: audioStream,
+            metadata: metadataStream,
+        }
+        this._send(request).then()
+        return {audioStream, metadataStream, requestId}
     }
 
 }
